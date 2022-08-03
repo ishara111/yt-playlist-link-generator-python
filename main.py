d5ef94140ad4c14d64edc0db7ccfaa8e2f44be42
ishara111 <isharagomes111@gmail.com>
2022-08-03 11:18:01 +0530

    title added

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#playlist link to csv
+from pytube import Playlist
+
+playlistUrl = input("Enter youtube playlist URL: ")
+p = Playlist(playlistUrl)
+print()
+print(f"Playlist Title: {p.title}")
+print()
