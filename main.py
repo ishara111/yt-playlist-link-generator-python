cd5d08c5764f52ec66d3fdf5e3e97508c7ac1095
ishara111 <isharagomes111@gmail.com>
2022-08-03 18:25:57 +0530

    final

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from pytube import Playlist
 
 playlistUrl = input("Enter youtube playlist URL: ")
@@ -6,5 +7,25 @@ print()
 print(f"Playlist Title: {p.title}")
 print()
 
-for url in p.video_urls:
-    print(url)
+original_stdout = sys.stdout
+rep = True
+while rep==True:
+    fileType = input("Enter file type csv or txt: ")
+    if fileType =="txt":
+        with open('playlist_links.txt', 'w') as f:
+            sys.stdout = f # Change the standard output to the file we created.
+            for url in p.video_urls:
+                print(url)
+            sys.stdout = original_stdout # Reset the standard output to its original value
+            break
+    elif fileType =="csv":
+        with open('playlist_links.csv', 'w') as f:
+            sys.stdout = f # Change the standard output to the file we created.
+            for url in p.video_urls:
+                print(url)
+            sys.stdout = original_stdout # Reset the standard output to its original value
+            break
+    else:
+        print("incorrect filetype")
+
+print("Done")
